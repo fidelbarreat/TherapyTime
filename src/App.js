699720185e573ae51e88e97a95cfbb48ce5c9eeb
yasminhammoud <yasmin.hammoud@correo.unimet.edu.ve>
2021-11-13 20:46:55 -0400

    slideshow-aboutus-homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import Slideshow from './components/Slideshow'
+import AboutUs from './components/AboutUs'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<main>
+			<Slideshow controles={true} autoplay={true} velocidad="3000" intervalo="10000"/>
+			<AboutUs />	
+		</main>
+	);
 }
 
 export default App;
